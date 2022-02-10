a3eb6d989b6d42491733453ecbd87b0012127ab3
hasan-ciftci <hasan.ciftci@path.com.tr>
2022-02-10 20:24:26 +0300

    bottom navbar added.

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:fsc_ibb/core/locator.dart';
 import 'package:fsc_ibb/widgets/category_menu_item/category_menu_item.dart';
@@ -18,84 +20,157 @@ class HomeView extends StatelessWidget {
             leading: const Icon(Icons.menu),
             actions: const [Icon(Icons.notifications), Icon(Icons.more_horiz)],
           ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
                   children: [
-                    const Flexible(
-                      child: TextField(
-                        decoration: InputDecoration(filled: true, fillColor: Colors.blue),
-                      ),
+                    Row(
+                      children: [
+                        const Flexible(
+                          child: TextField(
+                            decoration: InputDecoration(filled: true, fillColor: Colors.blue),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.orange,
+                        ),
+                      ],
                     ),
                     const SizedBox(
-                      width: 25,
+                      height: 25,
                     ),
-                    Container(
-                      width: 50,
-                      height: 50,
-                      color: Colors.orange,
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: const [
+                          CategoryMenuItem(),
+                          CategoryMenuItem(),
+                          CategoryMenuItem(),
+                          CategoryMenuItem(),
+                          CategoryMenuItem(),
+                        ],
+                      ),
                     ),
+                    const SizedBox(height: 25),
+                    const CarouselShowCard(),
+                    const SizedBox(height: 25),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Popular",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: const [
+                        ShowCard(),
+                        SizedBox(width: 25),
+                        ShowCard(),
+                      ]),
+                    ),
+                    const SizedBox(height: 25),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "You may like",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(children: const [
+                        ShowCard(),
+                        SizedBox(width: 25),
+                        ShowCard(),
+                      ]),
+                    ),
+                    SizedBox(height: MediaQuery.of(context).viewPadding.bottom + 125),
                   ],
                 ),
-                const SizedBox(
-                  height: 25,
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: const [
-                      CategoryMenuItem(),
-                      CategoryMenuItem(),
-                      CategoryMenuItem(),
-                      CategoryMenuItem(),
-                      CategoryMenuItem(),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewPadding.bottom + 25),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(25),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(
+                              sigmaX: 15.0,
+                              sigmaY: 15.0,
+                            ),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width - 25,
+                              padding: const EdgeInsets.all(16.0),
+                              color: Colors.grey.withOpacity(0.5),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: const [
+                                  MenuItem(
+                                    icon: Icons.home,
+                                    title: "Home",
+                                    isSelected: true,
+                                  ),
+                                  MenuItem(icon: Icons.play_arrow, title: "Live TV"),
+                                  MenuItem(icon: Icons.gamepad, title: "Games"),
+                                  MenuItem(icon: Icons.settings, title: "Settings"),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 25),
-                const CarouselShowCard(),
-                const SizedBox(height: 25),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Popular",
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(children: const [
-                    ShowCard(),
-                    SizedBox(width: 25),
-                    ShowCard(),
-                  ]),
-                ),
-                const SizedBox(height: 25),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "You may like",
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(children: const [
-                    ShowCard(),
-                    SizedBox(width: 25),
-                    ShowCard(),
-                  ]),
-                ),
-                SizedBox(height: MediaQuery.of(context).viewPadding.bottom+25),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
       viewModelBuilder: () => locator<HomeViewModel>(),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  const MenuItem({
+    Key key,
+    this.title,
+    this.icon,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: isSelected ? Colors.deepOrangeAccent : null),
+        const SizedBox(height: 8),
+        Text(
+          title,
+          style: Theme.of(context).textTheme.bodyText1.copyWith(color: isSelected ? Colors.deepOrangeAccent : null),
+        ),
+      ],
     );
   }
 }
